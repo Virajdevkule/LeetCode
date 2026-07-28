8ea896572e0f99f1f720ecf8ae28a3b5fcd7722b
Virajdevkule <devkuleviraj123@gmail.com>
2026-07-29 00:17:01 +0530

    Time: 29 ms (5.2%), Space: 49 MB (33.09%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,21 +1,29 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int m = nums1.length, n = nums2.length;
-        int[] merged = new int[m + n];
-        
-        System.arraycopy(nums1, 0, merged, 0, m);
-        System.arraycopy(nums2, 0, merged, m, n);
-        
-        Arrays.sort(merged);
-        
-        int total = merged.length;
-        int mid = total / 2;
-        
-        if (total % 2 == 1) {
-            return merged[mid];
-        } else {
-            return (merged[mid - 1] + merged[mid]) / 2.0;
+        int merged [] = new int [nums1.length + nums2.length];
+
+        for(int i = 0 ; i < nums1.length ; i++){
+            merged[i] = nums1[i];
+        } 
+        for ( int i = 0 ; i < nums2.length ; i++){
+            merged [nums1.length + i ] = nums2[i];
         }
         
+        for ( int i = 0 ; i < merged.length-1 ; i++){
+            for ( int j = 0 ; j < merged.length - 1 - i ; j++){
+                if ( merged [ j ] > merged [ j + 1 ]){
+                    int temp = merged[j];
+                    merged[j] = merged[j + 1 ];
+                    merged [j + 1 ] = temp ;
+                }
+            }
+
+        }
+        int n = merged.length ;
+        if ( n % 2 == 1){
+            return merged[ n / 2 ];
+        }else {
+            return ( merged [n/2 - 1 ] + merged [n / 2 ]) / 2.0 ;
+        }
     }
 }
